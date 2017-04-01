b79c308af34014a3e956983b47da31d5f5c416b8
Osama-Elsamny <oelsamny@sfu.ca>
2017-04-01 11:01:09 -0700

    Constructor , Destructor and Output operator
    Implementation of two constructor, empty destructor and the output
    operator as requested

--- a4-files/Customer.cpp
+++ a4-files/Customer.cpp
@@ -7,9 +7,19 @@
 
 #include "Customer.h"
 
-Customer::Customer(string x, char y, int z) {
+Customer::Customer(){
+	_lastName = "null";
+	_firstInitial = '\0';
+	_balance = 0;
+}
+Customer::Customer(string lastName,char firstInitial,int balance){
+	_lastName = lastName;
+	_firstInitial = firstInitial;
+	_balance = balance;
 }
 
 
+Customer::~Customer(){}
 ostream& operator<<(ostream & os,  Customer & c) {
+	os << c._lastName << ", " << c._firstInitial << ". (" << c._balance << ")";
 }
