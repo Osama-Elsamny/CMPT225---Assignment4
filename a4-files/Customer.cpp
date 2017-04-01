6debc2486d2aa24838f6093062e09e6f9142cb5a
Osama-Elsamny <oelsamny@sfu.ca>
2017-04-01 11:06:04 -0700

    Comparsion operators for Customers

--- a4-files/Customer.cpp
+++ a4-files/Customer.cpp
@@ -17,8 +17,72 @@ Customer::Customer(string lastName,char firstInitial,int balance){
 	_firstInitial = firstInitial;
 	_balance = balance;
 }
-
-
+bool Customer::operator< (const Customer& c) const{
+	if (this->_lastName > c._lastName)
+	{
+		return false;
+	}
+	if ((this->_lastName == c._lastName) && (this->_firstInitial >= c._firstInitial))
+	{
+		return false;
+	}
+	return true;
+}
+bool Customer::operator<= (const Customer& c) const{
+	if (this->_lastName > c._lastName)
+	{
+		return false;
+	}
+	if (this->_firstInitial > c._firstInitial)
+	{
+		return false;
+	}
+	return true;
+}
+bool Customer::operator> (const Customer& c) const{
+	if (this->_lastName < c._lastName)
+	{
+		return false;
+	}
+	if ((this->_lastName == c._lastName) && (this->_firstInitial <= c._firstInitial))
+	{
+		return false;
+	}
+	return true;
+}
+bool Customer::operator>= (const Customer& c) const{
+	if (this->_lastName < c._lastName)
+	{
+		return false;
+	}
+	if (this->_firstInitial < c._firstInitial)
+	{
+		return false;
+	}
+	return true;
+}
+bool Customer::operator== (const Customer& c) const{
+	if (this->_lastName != c._lastName)
+	{
+		return false;
+	}
+	if (this->_firstInitial != c._firstInitial)
+	{
+		return false;
+	}
+	return true;
+}
+bool Customer::operator!= (const Customer& c) const{
+	if (this->_lastName != c._lastName)
+	{
+		return true;
+	}
+	if (this->_firstInitial != c._firstInitial)
+	{
+		return true;
+	}
+	return false;
+}
 Customer::~Customer(){}
 ostream& operator<<(ostream & os,  Customer & c) {
 	os << c._lastName << ", " << c._firstInitial << ". (" << c._balance << ")";
